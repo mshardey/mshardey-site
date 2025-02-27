6d77fa73d90d6a8dd51d3fb286019edc7269837a
Mahmoud Shardey <tetteis@gmail.com>
2025-02-27 07:07:49 +0000

    Update content in config.ts with new configuration values

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  "Mahmoud Shardey | Personal Website & Blog";
+  "Mahmoud Shardey | Cloud Security Blog & Projects";
 export const SITE_DESCRIPTION =
-  "I am passionate about securing the cloud";
+  "Cloud Security Engineer with 10+ years in IT, sharing Azure tutorials, security projects, and insights on Defender, Sentinel, and automation.";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
